ce22f3b067556577cf1f3d1c194850cfd4290008
Your Name <you@example.com>
2024-01-05 16:04:30 +0700

    lab14_2.cpp completed

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -9,6 +9,7 @@ void findLocalMax(const double [][N], bool [][N]);
 
 void showMatrix(const bool [][N]);
 
+
 int main(){
 	double A[N][N]; 
 	bool B[N][N]; 
@@ -18,4 +19,38 @@ int main(){
 	cout << "Result is \n";
 	showMatrix(B);
 	return 0;
+}
+
+void showMatrix(const bool A[][N]){
+	for (int i = 0; i < N; i++){
+		for (int j = 0; j < N; j++){
+			cout << A[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
+void inputMatrix(double A[][N]){
+    for (int i = 0; i < N; ++i){
+    cout << "Row " << i+1 << ": ";
+        for (int j = 0; j < N; ++j){
+            cin >> A[i][j];
+        }
+    }
+}
+
+void findLocalMax(const double A[][N], bool B[][N]){
+	for (int i = 0; i < N; ++i){
+        B[i][0] = B[i][N - 1] = B[0][i] = B[N - 1][i] = 0;
+	}
+	for (int i = 1; i < N - 1; ++i) {
+        for (int j = 1; j < N - 1; ++j) {
+            if (A[i][j] >= A[i - 1][j] && A[i][j] >= A[i + 1][j] &&
+                A[i][j] >= A[i][j - 1] && A[i][j] >= A[i][j + 1]) {
+                B[i][j] = true; 
+            } else {
+                B[i][j] = false; 
+            }
+        }
+    }
 }
